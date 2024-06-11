c864fcd45adfba4a977e07d6bbcd578afa90e99f
Kmirror <Kmirror@outlook.com>
2024-06-12 00:28:41 +0800

    Feature: 1. remove some warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,8 +12,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-@Suppress("DEPRECATION")
+
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
